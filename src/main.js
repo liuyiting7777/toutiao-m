0a00aaa27b0f2b77fb3e0dfb2ae18fdfdc04da5a
liuyiting <liuyiting7777@163.com>
2022-02-22 20:31:19 +0800

    引入vant组件库

--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import router from './router'
 import store from './store'
 import './styles/index.less'
 
+// 加载vant核心组件库
+import Vant from '../node_modules/vant/es/index.js'
+// 加载vant全局样式
+import '../node_modules/vant/lib/index.css'
+
+// 注册使用vant组件
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
